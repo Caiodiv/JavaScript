29912bb931f7717ad5b8dfad36172a1d75dc0362
Caiodiv <84077829+Caiodiv@users.noreply.github.com>
2022-04-17 22:50:52 -0300

    Add files via upload

---
+++ Operações lógicas/index.js
@@ -0,0 +1,32 @@
+let spaceship = "Elemental"
+let velocity = 20
+
+// console.log(spaceship.length == 9 && velocity > 15)
+// Verdadeiro e Verdadeiro = Verdadeiro 
+
+// console.log( velocity > 10 && velocity < 19)
+// Verdadeiro e Falso = Falso 
+
+// console.log(velocity < 17 && spaceship == "Elemental")
+// Falso e Verdadeiro = Falso
+
+// console.log(spaceship == "Golias" && velocity > 21)
+// Falso e Falso = Falso
+
+// console.log(spaceship.length == 9 || velocity > 15)
+// Verdadeiro e Verdadeiro = Verdadeiro 
+
+// console.log( velocity > 10 || velocity < 19)
+// Verdadeiro e Falso = Verdadeiro 
+
+// console.log(velocity < 17 || spaceship == "Elemental")
+// Falso e Verdadeiro = Verdadeiro 
+
+// console.log(spaceship == "Golias" || velocity > 21)
+// Falso e Falso = Falso
+
+// console.log(!(velocity > 19))
+// NÃO Verdedeiro = Falso
+
+// console.log(!(spaceship == "Golias"))
+// NÃO Falso = Verdadeiro
